82a65f32bf80f278dd48ae1dc684230437d94cf0
githudbegazo <shanellabegazo@gmail.com>
2026-03-12 10:28:13 -0500

    coordenadas

---
+++ Semana_1/Actividades/Coordenada.java
@@ -0,0 +1,59 @@
+package pyPooAct1;
+
+public class Coordenada {
+    // Atributos
+    private double x;
+    private double y;
+
+    // Constructores
+    public Coordenada() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Coordenada(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coordenada(Coordenada c) {
+        this.x = c.x;
+        this.y = c.y;
+    }
+
+    // Setters
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    // Getters
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    // Métodos
+    public double distancia(Coordenada c) {
+        double dx = this.x - c.x;
+        double dy = this.y - c.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public static double distancia(Coordenada c1, Coordenada c2) {
+        double dx = c1.x - c2.x;
+        double dy = c1.y - c2.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
