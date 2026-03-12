e5bf4b173e6863c67dca51cb5340df49ab9a7e5f
try-69 <alexis.subia.luna@gmail.com>
2026-03-12 11:00:46 -0500

    Update Verificador.java

--- Semana_1/Actividades/Verificador.java
+++ Semana_1/Actividades/Verificador.java
@@ -2,7 +2,7 @@ package pyPooAct1;
 
 public class Verificador {
     public static Boolean esSobrePos (Rectangulo r1, Rectangulo r2) {
-        
+        return (r1.getEsquina1() < r2.getEsquina1() && r1.getEsquina2() < r2.getEsquina2());
     }
     public static Boolean esJunto (Rectangulo r1, Rectangulo r2){
 
@@ -11,4 +11,5 @@ public class Verificador {
 
     }
 }
+
 
