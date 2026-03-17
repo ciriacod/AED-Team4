c86baa7d6163aa5ef42cadee5acf7cf80255f579
try-69 <alexis.subia.luna@gmail.com>
2026-03-17 09:20:55 -0500

    Create Zona.java

---
+++ Semana_1/ejercicios2/Zona.java
@@ -0,0 +1,24 @@
+public class Zona {
+  private String mineral;
+  private double cantidad;
+  private double pureza;
+
+  public Zona(String mineral, double cantidad, double pureza) {
+    this.mineral = mineral;
+    this.cantidad = cantidad;
+    this.pureza = pureza;
+    }
+
+  public double calcularValor() {
+    return this.cantidad * this.pureza;
+  }
+
+  public String getMineral() {
+    return mineral;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + mineral + ", cantidad: " + cantidad + ", pureza: " + pureza + "]";
+  }
+}
