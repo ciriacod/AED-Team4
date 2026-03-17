24573c7ce0139e1b343cf4ad8174199675fe5075
githudbegazo <shanellabegazo@gmail.com>
2026-03-16 19:20:38 -0500

    Update Verificador.java

--- Semana_1/Actividades/Verificador.java
+++ Semana_1/Actividades/Verificador.java
@@ -1,15 +1,30 @@
 package pyPooAct1;
 
 public class Verificador {
-    public static Boolean esSobrePos (Rectangulo r1, Rectangulo r2) {
-        return (r1.getEsquina1() < r2.getEsquina1() && r1.getEsquina2() < r2.getEsquina2());
-    }
-    public static Boolean esJunto (Rectangulo r1, Rectangulo r2){
 
+    public static Boolean esSobrePos(Rectangulo r1, Rectangulo r2) {
+        return !(r1.getEsquina2().getX() < r2.getEsquina1().getX() ||  // r1 a la izquierda
+                 r1.getEsquina1().getX() > r2.getEsquina2().getX() ||  // r1 a la derecha
+                 r1.getEsquina2().getY() < r2.getEsquina1().getY() ||  // r1 abajo
+                 r1.getEsquina1().getY() > r2.getEsquina2().getY());   // r1 arriba
     }
-    public static Boolean esDisjunto (Rectangulo r1, Rectangulo r2){
 
+    // Se tocan en un borde
+    public static Boolean esJunto(Rectangulo r1, Rectangulo r2) {
+        boolean tocaHorizontal = (r1.getEsquina2().getX() == r2.getEsquina1().getX() ||
+                                  r2.getEsquina2().getX() == r1.getEsquina1().getX());
+        boolean tocaVertical   = (r1.getEsquina2().getY() == r2.getEsquina1().getY() ||
+                                  r2.getEsquina2().getY() == r1.getEsquina1().getY());
+
+        return (tocaHorizontal || tocaVertical) && !esSobrePos(r1, r2);
+    }
+
+    // Est separados
+    public static Boolean esDisjunto(Rectangulo r1, Rectangulo r2) {
+        return (r1.getEsquina2().getX() < r2.getEsquina1().getX() ||  // r1 completamente a la izquierda
+                r1.getEsquina1().getX() > r2.getEsquina2().getX() ||  // r1 completamente a la derecha
+                r1.getEsquina2().getY() < r2.getEsquina1().getY() ||  // r1 completamente abajo
+                r1.getEsquina1().getY() > r2.getEsquina2().getY());   // r1 completamente arriba
     }
 }
-
 
