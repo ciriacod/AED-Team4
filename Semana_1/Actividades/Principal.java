b3709de82dd113c650043edd5392e91757c2f553
ciriacod <61907129+ciriacod@users.noreply.github.com>
2026-03-17 14:16:32 +0000

    Creacion de Carpeta Secundaria

--- Semana_1/Actividades/Principal.java
+++ Semana_1/Actividades/Principal.java
@@ -1,41 +1,35 @@
-package pyPooAct1;
 import java.util.Scanner;
 
 public class Principal {
-
-
-
     public static void main(String[] args) 
     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese las coordenadas de la esquina inferior izquierda del primer rectangulo");
-        double x1_menor = scanner.nextDouble();
-        double y1_menor = scanner.nextDouble();
-        System.out.println("Ingrese la coordenadas de la esquina superior derecha del primer rectangulo");
-        double x1_mayor = scanner.nextDouble();
-        double y1_mayor = scanner.nextDouble();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Ingrese las coordenadas de la esquina inferior izquierda del primer rectangulo");
+            double x1_menor = scanner.nextDouble();
+            double y1_menor = scanner.nextDouble();
+            System.out.println("Ingrese la coordenadas de la esquina superior derecha del primer rectangulo");
+            double x1_mayor = scanner.nextDouble();
+            double y1_mayor = scanner.nextDouble();
 
-        System.out.println("Ingrese las coordenadas de la esquina inferior izquierda del segundo rectangulo");
-        double x2_menor = scanner.nextDouble();
-        double y2_menor = scanner.nextDouble();
-        System.out.println("Ingrese la coordenadas de la esquina superior derecha del segundo rectangulo");
-        double x2_mayor = scanner.nextDouble();
-        double y2_mayor = scanner.nextDouble();
+            System.out.println("Ingrese las coordenadas de la esquina inferior izquierda del segundo rectangulo");
+            double x2_menor = scanner.nextDouble();
+            double y2_menor = scanner.nextDouble();
+            System.out.println("Ingrese la coordenadas de la esquina superior derecha del segundo rectangulo");
+            double x2_mayor = scanner.nextDouble();
+            double y2_mayor = scanner.nextDouble();
 
-        Rectangulo rectangulo1 = new Rectangulo(new Coordenada(x1_menor, y1_menor), new Coordenada(x1_mayor, y1_mayor));
-        Rectangulo rectangulo2 = new Rectangulo(new Coordenada(x2_menor, y2_menor), new Coordenada(x2_mayor, y2_mayor));
-        if (Verificador.esSobrePos(rectangulo1, rectangulo2)) {
-            System.out.println("Los rectángulos están sobrepuestos.");
-        } else if (Verificador.esJunto(rectangulo1, rectangulo2)) {
-            System.out.println("Los rectángulos están juntos (tocan sus bordes).");
-        } else if (Verificador.esDisjunto(rectangulo1, rectangulo2)) {
-            System.out.println("Los rectángulos son disjuntos (separados).");
-        } else {
-            System.out.println("No se cumple ninguna condición específica.");
+            Rectangulo rectangulo1 = new Rectangulo(new Coordenada(x1_menor, y1_menor), new Coordenada(x1_mayor, y1_mayor));
+            Rectangulo rectangulo2 = new Rectangulo(new Coordenada(x2_menor, y2_menor), new Coordenada(x2_mayor, y2_mayor));
+            if (Verificador.esSobrePos(rectangulo1, rectangulo2)) {
+                System.out.println("Los rectángulos están sobrepuestos.");
+            } else if (Verificador.esJunto(rectangulo1, rectangulo2)) {
+                System.out.println("Los rectángulos están juntos (tocan sus bordes).");
+            } else if (Verificador.esDisjunto(rectangulo1, rectangulo2)) {
+                System.out.println("Los rectángulos son disjuntos (separados).");
+            } else {
+                System.out.println("No se cumple ninguna condición específica.");
+            }
         }
-
-
-
 
     }
 }
