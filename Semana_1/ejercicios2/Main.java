880fe7bbbbd6399267f57def2db4e7d256bd6287
try-69 <alexis.subia.luna@gmail.com>
2026-03-18 09:27:48 -0500

    Update Main.java

--- Semana_1/ejercicios2/Main.java
+++ Semana_1/ejercicios2/Main.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner lector = new Scanner(System.in);
-        AnalizadorMinero analizador = new AnalizadorMinero();
+        Scanner lector = new Scanner(System.in);                                         //Se instancia la clase Scanner
+        AnalizadorMinero analizador = new AnalizadorMinero();                            //Se instancia la clase AnalizadorMinero
         
-        try {
-            System.out.print("Ingrese la ruta del archivo (.txt): ");
-            String ruta = lector.nextLine();
-            analizador.cargarDesdeArchivo(ruta);
+        try {                        
+            System.out.print("Ingrese la ruta del archivo (.txt): ");                    
+            String ruta = lector.nextLine();                                              //Se ingresa por teclado la ruta del archivo
+            analizador.cargarDesdeArchivo(ruta);                                          //Se envia la ruta mediante un método de la clase AnalizadorMinero
             
             System.out.print("Ingrese el tamaño de la subregión (terreno de 3x3): ");
-            int k = lector.nextInt();
+            int k = lector.nextInt();                                                     //Se ingresa el tamaño de la subregión que se desea calcular
             
-            analizador.ejecutarAnalisis(k);
+            analizador.ejecutarAnalisis(k);                                               //Se envia el tamaño mediante un método
             
             } catch (Exception e) {
-                System.out.println("Error durante la ejecución: " + e.getMessage());
+                System.out.println("Error durante la ejecución: " + e.getMessage());      //Muestra un mensaje de error en caso que no se ejecuto correctamente
             } finally {
-                lector.close();
+                lector.close();                                                           //Se cierra Scanner
         }
     }
 }
