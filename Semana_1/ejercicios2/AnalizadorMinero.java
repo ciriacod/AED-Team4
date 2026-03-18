0ac005c4b04c34cacf280beeccb318f4b2b2f49e
try-69 <alexis.subia.luna@gmail.com>
2026-03-18 09:58:13 -0500

    Update AnalizadorMinero.java

--- Semana_1/ejercicios2/AnalizadorMinero.java
+++ Semana_1/ejercicios2/AnalizadorMinero.java
@@ -1,22 +1,22 @@
 package Semana_1.ejercicios2;
 
-import java.util.*;
-import java.io.*;
+import java.util.*;                                                                    //Se importan librerias escenciales
+import java.io.*;                                                                      
 
 public class AnalizadorMinero {
-  private Zona[][] terreno;
+  private Zona[][] terreno;                                                            //Se implementa Zona como un tipo de atributo
   private int filas;
   private int columnas;
   
-  public void cargarDesdeArchivo(String ruta) throws FileNotFoundException {
+  public void cargarDesdeArchivo(String ruta) throws FileNotFoundException {           //Método que permite la lectura de un archivo de texto
     Scanner sc = new Scanner(new File(ruta));
-    if (sc.hasNextInt()) {
-      this.filas = sc.nextInt();
+    if (sc.hasNextInt()) {                                                             //Lectura de la primera linea del contenido del archivo
+      this.filas = sc.nextInt();                                                       //almacenando en los atributos de la clase AnalizadorMinero
       this.columnas = sc.nextInt();
       this.terreno = new Zona[filas][columnas];
       
-      for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
+      for (int i = 0; i < filas; i++) {                                                //Lectura del contenido del archivo almacenandolo
+        for (int j = 0; j < columnas; j++) {                                           //en un arreglo formado con los atributos de la clase
           String min = sc.next();
           double cant = sc.nextDouble();
           double pur = sc.nextDouble();
@@ -24,42 +24,42 @@ public class AnalizadorMinero {
         }
       }
     }
-    sc.close();
+    sc.close();                                                                        //Cierre de la clase Scanner
   }
   
-  public void ejecutarAnalisis(int k) {
-    if (k > filas || k > columnas || k <= 0) {
-        System.out.println("Error: Tamaño no permitido");
+  public void ejecutarAnalisis(int k) {                                                //Método que permite el analizis de una zona segun un valor ingresado
+    if (k > filas || k > columnas || k <= 0) {                                         //Validación del número ingresado para que este no sea
+        System.out.println("Error: Tamaño no permitido");                              //mayor al perimetro total del área minera
         return;
     }
     double valorMaximo = -1;
     int mejorF = 0, mejorC = 0;
     
-    for (int i = 0; i <= filas - k; i++) {
+    for (int i = 0; i <= filas - k; i++) {                                             //Bucle que analiza todo un terreno segun el valor ingresado
       for (int j = 0; j <= columnas - k; j++) {
-        double sumaActual = 0;
-        for (int r = i; r < i + k; r++) {
+        double sumaActual = 0;                                                         //Se define una variable para determinar la suma total del terreno
+        for (int r = i; r < i + k; r++) {                                              //Bucle que permite moverse dentro del arreglo
           for (int c = j; c < j + k; c++) {
-            sumaActual += terreno[r][c].calcularValor();
+            sumaActual += terreno[r][c].calcularValor();                               //Se almacena cada calcula de la zona del terreno en la variable definida
           }
         }
-        if (sumaActual > valorMaximo) {
+        if (sumaActual > valorMaximo) {                                                //Condicional que determina si la suma anterior es mayor que la suma actual
           valorMaximo = sumaActual;
           mejorF = i;
           mejorC = j;
         }
       }
     }
-    imprimirReporte(mejorF, mejorC, k, valorMaximo);
+    imprimirReporte(mejorF, mejorC, k, valorMaximo);                                   //Se invoca un metodo para mostrar los resultados
   }
   
-  private void imprimirReporte(int f, int c, int k, double total) {
+  private void imprimirReporte(int f, int c, int k, double total) {                    //Método que muestra los resultados del análizis
     System.out.println("\nRegión más valiosa encontrada:");
     System.out.println("Posición inicial: (" + f + "," + c + ")");
     System.out.println("Tamaño de la región: " + k + "x" + k);
     System.out.println("Zonas analizadas:");
     
-    Map<String, Integer> conteo = new HashMap<>(); 
+    Map<String, Integer> conteo = new HashMap<>();                                      //Se almacena la informacion dentro de un Map para mostrarlo por consola
     
     for (int i = f; i < f + k; i++) {
         for (int j = c; j < c + k; j++) {
