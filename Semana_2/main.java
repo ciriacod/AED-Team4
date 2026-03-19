848123b772e73e57f6c20e088e1cfcae77aed189
try-69 <alexis.subia.luna@gmail.com>
2026-03-19 10:32:18 -0500

    Update main.java

--- Semana_2/main.java
+++ Semana_2/main.java
@@ -2,6 +2,6 @@ package Semana_2;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("¡Hola, Mundo!");
+        System.out.println("¡Hola, adios!");
     }
 }
