3c3ac8eaaa444a9a2df8c63b81f94fab0b1b2dfd
githudbegazo <shanellabegazo@gmail.com>
2026-03-18 20:09:00 -0500

    Refactor Rectangulo class with added comments

--- Semana_1/ejercicios1/Rectangulo.java
+++ Semana_1/ejercicios1/Rectangulo.java
@@ -1,14 +1,15 @@
 package Semana_1.ejercicios1;
 
 public class Rectangulo {
+        //atributos
         private Coordenada esquina1;
         private Coordenada esquina2;
-
+//constructor parametrizado
         public Rectangulo(Coordenada c1, Coordenada c2) {
             this.esquina1 = c1;
             this.esquina2 = c2;
         }
-
+//metodos get y set
         public void setEsquina1(Coordenada coo) {
             this.esquina1 = coo;
         }
@@ -24,11 +25,11 @@ public class Rectangulo {
         public Coordenada getEsquina2() {
             return this.esquina2;
         }
-
+//metodo para mostrar
         public String toString() {
             return "(" + this.esquina1.toString() + ", " + this.esquina2.toString() + ")";
         }
-        
+        //calculos que pidieron
         public double calcularArea() {
             double base = Math.abs(esquina2.getX() - esquina1.getX());
             double altura = Math.abs(esquina2.getY() - esquina1.getY());
