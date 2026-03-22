aa185ab471d6707a0212986570f437e3c3172ca6
try-69 <alexis.subia.luna@gmail.com>
2026-03-22 17:24:41 -0500

    Add files via upload

---
+++ Semana_2/Ejercicios/Ejercicio8/Objecto.java
@@ -0,0 +1,14 @@
+package com.Ejercicio8;
+
+public class Objecto {
+    private String name;
+    
+    public Objecto(String name){
+        this.name = name;
+    }
+    
+    @Override
+    public String toString(){
+        return name;
+    }
+}
