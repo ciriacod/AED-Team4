86aa0c8fbe0c6eb91ddb3ab2db76faa0e55b5c72
githudbegazo <shanellabegazo@gmail.com>
2026-03-23 19:08:37 -0500

    Update  Smartphone.java

--- Semana_2/Ejercicios/Ejercicio9/ Smartphone.java
+++ Semana_2/Ejercicios/Ejercicio9/ Smartphone.java
@@ -1,49 +1,94 @@
 package ejercicio9;
 
-public class Smartphone implements Cargable 
+public class Smartphone implements Cargable
+//Clase que representa un dispositivo Smartphone que implementa la interfaz Cargable
 {
 	private double consumoVatios;
 	private String modelo;
-	
-	Smartphone ( String modelo,double consumoVatios){
-		this.consumoVatios=consumoVatios;
-		this.modelo=modelo;
+	private int nivelBateria;
+	// Clase que representa un dispositivo Smartphone que implementa la interfaz Cargable en 0
+	Smartphone(String modelo, double consumoVatios)
+	{
+		this.consumoVatios = consumoVatios;
+		this.modelo = modelo;
+		this.nivelBateria = 0;
+	}
+	//set y get
+	public void setconsumoVatios(double consumoVatios)
+	{
+		this.consumoVatios = consumoVatios;
 	}
 	
-	public void setconsumoVatios(double consumoVatios) {
-		this.consumoVatios=consumoVatios;
+	public void setmodelo(String modelo)
+	{
+		this.modelo = modelo;
 	}
-	public void setconsumoVatios(String modelo) {
-		this.modelo=modelo;
-	} 
+	
+	public void setNivelBateria(int nivelBateria)
+	{
+		this.nivelBateria = nivelBateria;
+	}
+	
 	@Override
-	public double getConsumoVatios() {
+	public double getConsumoVatios()
+	{
 		return consumoVatios;
-		} 
-	public String getmodelo() {
+	}
+	
+	public String getmodelo()
+	{
 		return modelo;
 	}
+	
 	@Override
-	public int getNivelBateria() 
+	public int getNivelBateria()
 	{
-		return ((int)consumoVatios *100) ;
-		
+		return nivelBateria;
 	}
-	//cuanta cantidad 
+	//// Método que incrementa la batería del dispositivo
 	@Override
-	public void cargar(int cantidad) {
-		if(cantidad<100) {
-			System.out.println("cargando ...");
-		}else 
+	public void cargar(int cantidad)
+	{
+		nivelBateria += cantidad;
+		//// Método que incrementa la batería del dispositivo
+		
+		if(nivelBateria > 100)
 		{
-			System.out.println("Esta al 100% ");
-		}
+			nivelBateria = 100;
+		}	// Control para que no supere el 100%
+		
+		System.out.println("bateria del smartphone: " + nivelBateria + "%");
 	}
 	
 	@Override
-	public String toString() {
-		return "-Consumo: "+consumoVatios + " -Modelo" + modelo;
-		
+	public String toString()
+	{
+		return "-Consumo: " + consumoVatios + " -Modelo: " + modelo + " -Bateria: " + nivelBateria + "%";
 	}
-
+	
+	@Override
+	public boolean equals(Object obj)// Método equals para comparar dos objetos Smartphone
+	// Permite verificar si tienen el mismo modelo y consumo
+	{
+		if(this == obj)
+		{
+			return true;
+		}
+		
+		if(obj == null || getClass() != obj.getClass())
+		{
+			return false;
+		}
+		
+		Smartphone otro = (Smartphone) obj;
+		
+		return this.modelo.equals(otro.modelo) && this.consumoVatios == otro.consumoVatios;
+	}
+	// Método hashCode que genera un código hash basado en los atributos del objeto
+	// Debe ser consistente con equals para garantizar correcto funcionamiento en colecciones
+	@Override
+	public int hashCode() {
+	    return modelo.hashCode() + (int) consumoVatios;
+	}
 }
+
