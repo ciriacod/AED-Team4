f8862f2bd4f5dfc6a3c4103538d681d09c78ea50
githudbegazo <shanellabegazo@gmail.com>
2026-04-06 17:52:28 -0500

    Add method to count even numbers in an array

--- Semana_3/actividad/par.java
+++ Semana_3/actividad/par.java
@@ -1,1 +1,25 @@
+package complejidadalgoritmica;
+
+public class ContarPares {
+	
+	public static int contarPares(int[]arreglo ) {
+		int cont=0; //O(1)
+		for(int i=0;i < arreglo.length;i++) //O(n)
+		{
+			if(arreglo[i] % 2==0) //O(1)
+			{
+				cont+=1; //O(1)
+				
+			}
+			
+		}return cont; //O(1)
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] n= {1,2,3,4,6,9,10};
+		System.out.print("existen numeros pares:"+contarPares(n));
+	}
+
+}
 
