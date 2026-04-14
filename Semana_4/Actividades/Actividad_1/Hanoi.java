ada1fdc5bdd9991bb5b1266cfe531d98848362a0
ciriacod <61907129+ciriacod@users.noreply.github.com>
2026-04-14 13:49:03 +0000

    Add Hanoi class with recursive solution for Tower of Hanoi problem

---
+++ Semana_4/Actividades/Actividad_1/Hanoi.java
@@ -0,0 +1,18 @@
+public class Hanoi {
+            //Creando el metodo recursivo para la solucion de las torres hanoi
+        public static void torresHanoi(int discos, int torre1, int torre2, int torre3){
+            //Caso base
+            if (discos == 1){
+                System.out.println("Mover disco de torre " + torre1 + " a torre " + torre3);
+            } else{
+            //Dominio (problema – 1)
+                torresHanoi(discos -1, torre1, torre3, torre2);
+                System.out.println("mover disco de torre "+ torre1 + " a torre " + torre3);
+                torresHanoi(discos -1, torre2, torre1, torre3);
+            }
+    }
+
+        public static void main(String[] args) {
+        torresHanoi(3, 1, 2, 3);
+    }
+}
