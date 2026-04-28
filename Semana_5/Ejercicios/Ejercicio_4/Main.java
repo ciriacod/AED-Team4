693946eec1960fd5929b1b0be2eccca643bf93d9
githudbegazo <shanellabegazo@gmail.com>
2026-04-27 20:59:14 -0500

    Refactor Main.java to implement linked list functionality

--- Semana_5/Ejercicios/Ejercicio_4/Main.java
+++ Semana_5/Ejercicios/Ejercicio_4/Main.java
@@ -1,5 +1,86 @@
-package Semana_5.Ejercicios.Ejercicio_4;
+package LAB5DOS;
 
-public class Main {
-    
+// Clase Nodo generico
+class Node<T> {
+    T data;          // dato almacenado en el nodo
+    Node<T> next;    // referencia al siguiente nodo
+
+    Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// Clase Lista Enlazada
+class ListLinked<T> {
+    Node<T> head;    // referencia al primer nodo de la lista
+
+    ListLinked() {
+        this.head = null;
+    }
+
+    // Verifica si la lista esta vacia
+    boolean isEmptyList() {
+        return head == null;
+    }
+
+    // Inserta un elemento al inicio de la lista
+    void insertFirst(T x) {
+        Node<T> temp = new Node<>(x);
+        temp.next = head;
+        head = temp;
+    }
+
+    // Inserta un elemento al final de la lista
+    void insertLast(T x) {
+        Node<T> newNode = new Node<>(x);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> curr = head;
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
+        }
+    }
+
+    // Imprime todos los elementos de la lista
+    void print() {
+        Node<T> curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+}
+
+// Clase InsertarFinal
+class InsertarFinal<T> {
+    // Metodo estatico que inserta directamente al final de la lista original
+    public static <T> void insertarFinal(ListLinked<T> lista, T x) {
+        lista.insertLast(x);
+    }
+}
+
+// Clase principal para probar
+public class ejer3 {
+    public static void main(String[] args) {
+        ListLinked<Integer> lista = new ListLinked<>();
+
+        // Insertamos elementos al inicio
+        lista.insertFirst(10);
+        lista.insertFirst(20);
+        lista.insertFirst(30);
+
+        System.out.println("Lista original:");
+        lista.print();
+
+        // Insertamos un elemento al final
+        InsertarFinal.insertarFinal(lista, 40);
+
+        System.out.println("Lista con elemento al final:");
+        lista.print();
+    }
 }
