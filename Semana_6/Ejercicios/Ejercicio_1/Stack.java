63762a3b440a34ecb3b87f8e01e765c15b2c4c55
Sebastian1029912 <sebastian.alvarezb@estudiante.ucsm.edu.pe>
2026-05-04 19:27:12 -0500

    Create Stack.java

---
+++ Semana_6/Ejercicios/Ejercicio_1/Stack.java
@@ -0,0 +1,8 @@
+package actividadPilaLista;
+
+public interface Stack<E> {
+    void push(E x);
+    E pop() throws ExceptionIsEmpty;
+    E top() throws ExceptionIsEmpty;
+    boolean isEmpty();
+}
