adff99b2df6fe61c86c10998d17ef5c1bc23decc
githudbegazo <shanellabegazo@gmail.com>
2026-05-05 06:30:56 -0500

    Refactor Node class with comments and formatting

--- Semana_6/Ejercicios/Ejercicio_1/Node.java
+++ Semana_6/Ejercicios/Ejercicio_1/Node.java
@@ -1,15 +1,15 @@
 package Semana_6.Ejercicios.Ejercicio_1;
 
-public class Node<E> {
-    private E data;
-    private Node<E> next;
+public class Node<E> { //creamos una clase nodo con atributos dentro
+    private E data;    //dato de tipo generico
+    private Node<E> next;    //creamos una referencia de tipo nodo llamado siguiente
 
-    public Node(E data) {
+    public Node(E data) {        //constructores
         this.data = data;
-        this.next = null;
+        this.next = null;        //nodo siguiente sera nulo
     }
 
-    public E getData() { return data; }
-    public Node<E> getNext() { return next; }
-    public void setNext(Node<E> next) { this.next = next; }
+    public E getData() { return data; }            //metodo getfata para mostrar dato
+    public Node<E> getNext() { return next; }        //metodo next de tipo objeto node reotrnara siguiente
+    public void setNext(Node<E> next) { this.next = next; }        //metodo void que ingresaremos un parametro de tipo nodo siguiente para cambiar el valor del nodo next
 }
