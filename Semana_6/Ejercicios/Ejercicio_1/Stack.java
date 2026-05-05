9cb08a51994ec19c10f53dce29c3835195d66cc5
githudbegazo <shanellabegazo@gmail.com>
2026-05-05 06:34:49 -0500

    Update Stack interface method comments for clarity

--- Semana_6/Ejercicios/Ejercicio_1/Stack.java
+++ Semana_6/Ejercicios/Ejercicio_1/Stack.java
@@ -1,8 +1,8 @@
 package Semana_6.Ejercicios.Ejercicio_1;
 
 public interface Stack<E> { //Implementamos una interfaz con los metodos
-    void push(E x);        //ingresar elemento al final de tipo generico
-    E pop() throws ExceptionIsEmpty;        //metodo sacar nodo del inicio
-    E top() throws ExceptionIsEmpty;        //metodo mostrar el tope o rear
+    void push(E x);        //ingresar elemento en el tope
+    E pop() throws ExceptionIsEmpty;        //metodo sacar el ultimo elemento ingresado
+    E top() throws ExceptionIsEmpty;        //metodo mostrar el tope
     boolean isEmpty();
 }
