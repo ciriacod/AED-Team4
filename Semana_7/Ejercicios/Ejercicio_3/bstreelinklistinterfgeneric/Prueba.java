a4a8b21d810974381992c5879266295d20b4c272
try-69 <alexis.subia.luna@gmail.com>
2026-05-11 20:21:35 -0500

    Update Prueba.java

--- Semana_7/Ejercicios/Ejercicio_3/bstreelinklistinterfgeneric/Prueba.java
+++ Semana_7/Ejercicios/Ejercicio_3/bstreelinklistinterfgeneric/Prueba.java
@@ -1,13 +1,19 @@
 
-package bstreelinklistinterfgeneric3;
+package bstreelinklistinterfgeneric;
 
 public class Prueba {
     
     // Compara si dos arboles son iguales en area
-    public static <T extends Comparable<T>>
-        boolean sameArea(LinkedBST<T> tree1,
-                         LinkedBST<T> tree2) {
-
-            return tree1.areaBST() == tree2.areaBST();
-        }
+    public static <T extends Comparable<T>> boolean sameArea(LinkedBST<T> tree1, LinkedBST<T> tree2) {
+        return tree1.areaBST() == tree2.areaBST();
+    }
+    
+    // Imprime las estadisticas del arbol    
+    public static void printTreeStats(String nombre, LinkedBST<Integer> tree) {
+        System.out.println(">>> " + nombre);
+        System.out.println("Estructura visual:");
+        System.out.println(tree.toString()); // Usa tu buildString recursivo
+        System.out.println("Resultado areaBST(): " + tree.areaBST());
+        System.out.println("----------------------------------------\n");
+    }
 }
