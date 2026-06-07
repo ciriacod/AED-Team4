732f89bb68da498c13a2450ee7291761e7d3051c
githudbegazo <shanellabegazo@gmail.com>
2026-06-07 17:50:19 -0500

    Create Libro.java

---
+++ Semana_9/btree/Ejercicios/Ejercicio04/Libro.java
@@ -0,0 +1,29 @@
+package Ejercicio04;
+
+public class Libro implements Comparable<Libro> {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private int anio;
+
+    public Libro(String isbn, String titulo, String autor, int anio) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    @Override
+    public int compareTo(Libro otro) {
+        return this.isbn.compareTo(otro.isbn);
+    }
+
+    @Override
+    public String toString() {
+        return isbn + " - " + titulo + " - " + autor + " - " + anio;
+    }
+}
