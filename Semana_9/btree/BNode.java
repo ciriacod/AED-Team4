b546fb88c0497ae50a30e3718121b02ad3d92d25
githudbegazo <shanellabegazo@gmail.com>
2026-06-08 09:34:00 -0500

    Update BNode.java

--- Semana_9/btree/BNode.java
+++ Semana_9/btree/BNode.java
@@ -3,7 +3,7 @@ package Semana_9.btree;
 import java.util.ArrayList;
 
 
-public class BNode <E extends  Comparable <E>>{{
+public class BNode <E extends  Comparable <E>>{
     protected ArrayList<E> keys;
     protected ArrayList<BNode<E>> childs;
     protected int count;
@@ -33,19 +33,19 @@ public class BNode <E extends  Comparable <E>>{{
         return count == 0;
     }
 
-    public boolean searchNode(E bus, int[] pos){
+    public boolean searchNode(E bus, int[] pos) {
         int i = 0;
-        // Avanzamos mientras el elemento actual sea menor al que buscamos
-        while (i < this.count && ((Comparable<E>) this.keys.get(i)).compareTo(bus) < 0) {
+
+        while (i < this.count && this.keys.get(i).compareTo(bus) < 0) {
             i++;
         }
-        
-        pos[0] = i; // Guardamos la posicion donde se quedo
 
-        // Si no se paso del conteo y es exactamente igual, lo encontro
-        if (i < this.count && this.keys.get(i).equals(bus)) {
+        pos[0] = i;
+
+        if (i < this.count && this.keys.get(i).compareTo(bus) == 0) {
             return true;
         }
+
         return false;
     }
 
