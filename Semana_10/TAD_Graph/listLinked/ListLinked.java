ac5c3bfc47bc8ef35bcbdfdd8816fc09659afdd8
ciriacod <caico2009yepes@hotmail.com>
2026-06-11 09:07:11 -0500

    Implement ListLinked methods: complete isEmpty, size, insert, search, remove, clear, and enhance toString for better output

--- Semana_10/TAD_Graph/listLinked/ListLinked.java
+++ Semana_10/TAD_Graph/listLinked/ListLinked.java
@@ -3,41 +3,82 @@ package Semana_10.TAD_Graph.listLinked;
 import Importar.IEstructuras.List;
 
 public class ListLinked<T extends Comparable<T>> implements List<T> {
+    Node<T> root;
+    int size;
+
+    public ListLinked() {
+        this.root = null;
+        this.size = 0;
+    }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+        return size == 0;
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        return size;
     }
 
     @Override
     public void insert(T data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+        Node<T> newNodo = new Node<T>(data);
+        newNodo.setNext(root);
+        root = newNodo;
+        size++;
     }
 
     @Override
     public boolean search(T data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'search'");
+        Node<T> find = root;
+        while (find != null) {
+            if (find.getData().compareTo(data) == 0)
+                return true;
+            find = find.getNext();
+        }
+        return false;
     }
 
     @Override
     public void remove(T data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+        if (isEmpty()) return;
+
+        // Caso 1: El dato esta en el primer nodo (root)
+        if (root.getData().compareTo(data) == 0) {
+            root = root.getNext();
+            size--;
+            return;
+        }
+
+        // Caso 2: El dato esta mas adelante
+        Node<T> delete = root;
+        while (delete.getNext() != null) {
+            if (delete.getNext().getData().compareTo(data) == 0) {
+                delete.setNext(delete.getNext().getNext());
+                size--;
+                return; // Terminamos para evitar NullPointerException
+            }
+            delete = delete.getNext();
+        }
     }
 
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+        root = null;
+        size = 0;
     }
-    
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        Node<T> current = root;
+        while (current != null) {
+            sb.append(current.getData());
+            if (current.getNext() != null) sb.append(", ");
+            current = current.getNext();
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
